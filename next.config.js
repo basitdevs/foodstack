82965c8e977f3f59ecc14f2a065b884082535c98
basitdevs <arsajid090@gmail.com>
2025-03-22 20:46:09 +0500

    fixing deploy isseus

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
-
-
 module.exports = {
     reactStrictMode: true,
+    staticPageGenerationTimeout: 300, // Increase timeout to 5 minutes
     images: {
         domains: ['stackfood.6am.one'],
-       // staticPageGenerationTimeout: 1500//Domain of image host
+        // staticPageGenerationTimeout: 1500//Domain of image host
     },
 }
